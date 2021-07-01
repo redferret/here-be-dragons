1a961157cb4c8cc21dddafa281337ff06209ac17
Richard <rdesilvey@gmail.com>
2021-07-01 11:44:00 -0600

    Finish examples

--- spec/nytimes_spec.rb
+++ spec/nytimes_spec.rb
@@ -9,14 +9,16 @@ RSpec.describe Nytimes do
 
   it 'test_it_can_get_copyright' do
     #Set your code to the local variable, "result"
-    result = @hash['copyright']
-    
+    result = @hash[:copyright]
+
     expect(result).to eq "Copyright (c) 2018 The New York Times Company. All Rights Reserved."
   end
 
   it 'test_it_can_get_array_of_stories' do
     #Set your code to the local variable, "result"
 
+    result = @hash[:results]
+    
     expect(result).to be_an Array
     expect(result.count).to eq 44
   end
@@ -24,10 +26,15 @@ RSpec.describe Nytimes do
   it 'test_it_can_get_all_stories_with_subsection_of_politics' do
     #Set your code to the local variable, "result"
 
+    results = @hash[:results]
+    result = results.find_all do |r|
+      r[:subsection] == 'Politics'
+    end
+    
     expect(result).to be_an Array
     expect(result.count).to eq 6
-    expect(result.first['title']).to eq "Congressional G.O.P. Agenda Quietly Falls Into Place Even as Trump Steals the Spotlight"
-    expect(result.last['title']).to eq "Conspiracy Theories Made Alex Jones Very Rich. They May Bring Him Down."
+    expect(result.first[:title]).to eq "Congressional G.O.P. Agenda Quietly Falls Into Place Even as Trump Steals the Spotlight"
+    expect(result.last[:title]).to eq "Conspiracy Theories Made Alex Jones Very Rich. They May Bring Him Down."
   end
 
 end
